ef6b10fbdba3243f4cc78d4ec7bdeb3343951619
Yellokat <jenova195@gmail.com>
2025-03-21 00:22:12 +0200

    feat : part 10 - exercise 12

--- frontend/src/utils/apolloClient.js
+++ frontend/src/utils/apolloClient.js
@@ -1,9 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
-
+import Constants from 'expo-constants';
 
 const createApolloClient = () => {
   return new ApolloClient({
-    uri: 'http://192.168.0.189:4000/graphql',
+    uri: Constants.expoConfig.extra.apolloUri,
     cache: new InMemoryCache(),
   });
 };
