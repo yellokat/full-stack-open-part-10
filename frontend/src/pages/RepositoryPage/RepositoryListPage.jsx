deb961831abfa24ec6987096aa56ab902a293f48
Yellokat <jenova195@gmail.com>
2025-03-23 02:21:16 +0200

    feat : published search bar component for exercise 10-24

--- frontend/src/pages/RepositoryPage/RepositoryListPage.jsx
+++ frontend/src/pages/RepositoryPage/RepositoryListPage.jsx
@@ -5,27 +5,34 @@ import {useEffect, useState} from "react";
 import useRepositories from "../../hooks/useRepositories";
 import {useNavigate} from "react-router-native";
 import {Picker} from '@react-native-picker/picker';
+import {Searchbar} from 'react-native-paper';
 
 const styles = StyleSheet.create({
   separator: {
     height: 10,
     backgroundColor: theme.colors.backgroundGrey,
   },
+  container: {
+    marginHorizontal: 20,
+    marginVertical: 10
+  }
 });
 
 const ItemSeparator = () => <View style={styles.separator}/>;
 
-export const RepositoryListContainer = ({repositories, onPressed, pickerMenu}) => {
+export const RepositoryListContainer = ({repositories, onPressed, headerComponent}) => {
 
   return (
     <FlatList
       data={repositories}
       ItemSeparatorComponent={ItemSeparator}
-      ListHeaderComponent={pickerMenu}
+      ListHeaderComponent={headerComponent}
       renderItem={
         ({item}) => {
           return (
-            <Pressable onPress={()=>{onPressed({path: `/${item.id}`})}}>
+            <Pressable onPress={() => {
+              onPressed({path: `/${item.id}`})
+            }}>
               <RepositoryItem {...item}/>
             </Pressable>
           )
@@ -39,6 +46,7 @@ const RepositoryListPage = () => {
   const navigate = useNavigate();
   const [sortBy, setSortBy] = useState();
   const {repositories} = useRepositories(sortBy)
+  const [searchPhrase, setSearchPhrase] = useState('')
 
   if (!repositories) {
     return null;
@@ -51,7 +59,7 @@ const RepositoryListPage = () => {
   const SortByPickerMenu = () => {
     return <Picker
       selectedValue={sortBy}
-      onValueChange={(itemValue, _) =>{
+      onValueChange={(itemValue, _) => {
         setSortBy(itemValue)
       }}>
       <Picker.Item label="Latest Repositories" value="latest"/>
@@ -60,8 +68,25 @@ const RepositoryListPage = () => {
     </Picker>
   }
 
+  const SearchBarMenu = () => {
+    return <Searchbar
+      placeholder="Search repositories..."
+      onChangeText={setSearchPhrase}
+      value={searchPhrase}
+    />
+  }
+
+  const HeaderComponent = () => {
+    return <>
+      <View style={styles.container}>
+        <SearchBarMenu/>
+      </View>
+      <SortByPickerMenu/>
+    </>
+  }
+
   return (
-    <RepositoryListContainer repositories={repositories} onPressed={onPressed} pickerMenu={<SortByPickerMenu/>}/>
+    <RepositoryListContainer repositories={repositories} onPressed={onPressed} headerComponent={<HeaderComponent/>}/>
   );
 };
 
